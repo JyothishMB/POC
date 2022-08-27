b09923dca732e6f7cc81ba3ee8b2050f7575901f
JyothishMB <jyothishmb1992@gmail.com>
2022-08-27 09:25:00 +0530

    defenition of DIP added as comment.

--- SOLID/DIP/Program.cs
+++ SOLID/DIP/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+The Dependency Inversion Principle (DIP) states that 
+a high-level class must not depend upon a lower level class. 
+They must both depend upon abstractions. And, secondly, 
+an abstraction must not depend upon details, but the details 
+must depend upon abstractions.
+*/
 Console.WriteLine("Hello, World!");
