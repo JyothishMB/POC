6a94fb2383c54ef6c2701abb3b9e8fa7ee652807
JyothishMB <jyothishmb1992@gmail.com>
2022-10-01 22:00:48 +0530

    Folder changes and card addition

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
+  const expenseslist = [
+    {
+      id: 1,
+      title: 'Parker Pen',
+      amount: 30,
+      date: new Date(2023, 4, 29)
+    },
+    {
+      id: 1,
+      title: 'Swis Knife',
+      amount: 400,
+      date: new Date(2023, 3, 21)
+    },
+    {
+      id: 1,
+      title: 'Trecking Pants',
+      amount: 500,
+      date: new Date(2023, 2, 16)
+    }
+  ]
   return (
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses expenses={expenseslist}/>
     </div>
   );
 }
---
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <h2 className="expense-item__price">${props.amount}</h2>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
