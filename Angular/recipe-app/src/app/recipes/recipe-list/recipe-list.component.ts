0b0cf98895bc02c7455eb21ef42199bfe3405f0b
JyothishMB <jyothishmb1992@gmail.com>
2022-10-09 16:07:07 +0530

    commit before item to detail communication

--- Angular/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,9 +12,9 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Grilled Salmon',
       'Salmon grilled with ginger and garlic with a slight rosemary toping',
       "https://149777215.v2.pressablecdn.com/wp-content/uploads/2007/09/shutterstock_248850307.jpg"),
-      new Recipe('Grilled Salmon',
-      'Salmon grilled with ginger and garlic with a slight rosemary toping',
-      "https://149777215.v2.pressablecdn.com/wp-content/uploads/2007/09/shutterstock_248850307.jpg")
+      new Recipe('Garlic Pork chops',
+      'Garlic pork chops in creamy mushroom sauce',
+      "https://www.eatwell101.com/wp-content/uploads/2019/02/Garlic-Pork-Chops-in-Creamy-Mushroom-Sauce-Recipe.jpg")
   ];
 
   constructor() { }
